68d626bbf63301b46d964ea786c3dc601a5c3645
Michael Tse <Michaeltessey1@gmail.com>
2019-02-16 15:48:52 -0500

    started menu component

---
+++ thai-street-app/src/Menu/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Menu.css'
+
+const Menu = () => {
+    return (
+        <div className="Rectangle">
+            <h2>Menu</h2>
+            <div>content</div>
+        </div>
+    )
+}
+
+export default Menu
