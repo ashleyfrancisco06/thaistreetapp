0f2bc31f3470bede979d919768cf2c8208fe3a00
Ashley Francisco <Francisco.ashley1@gmail.com>
2019-02-18 18:03:34 -0500

    fixed links in nav

--- thai-street-app/src/Header/Header.js
+++ thai-street-app/src/Header/Header.js
@@ -5,25 +5,25 @@ import { Link } from 'react-router-dom'
 import Footer from '../Footer/Footer.js'
 
 const Header = () => {
-    return (
-      <header>
-        <h1 className="Thai-Street">
-          <Link to="/">Thai Street</Link>
-        </h1>
-        <nav>
-          <li className="menu">
-            <Link to="/menu">Menu</Link>
-          </li>
-          <li className="about">About</li>
-          <li className="order">
-            <a href="https://www.beyondmenu.com/29547/new-york/rhong-tiam-new-york-10010.aspx?utm_source=satellite&utm_medium=menu_group#group_609819">
-              Order Online
+  return (
+    <header>
+      <h1 className="Thai-Street">
+        <Link to="/">Thai Street</Link>
+      </h1>
+      <nav>
+        <li className="menu">
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li className="about">About</li>
+        <li className="order">
+          <a href="https://www.seamless.com/menu/thai-street-31st-e-21st-st--21-new-york/975663">
+            Order Online
             </a>
-          </li>
-        </nav>
-        <Footer />
-      </header>
-    );
+        </li>
+      </nav>
+      <Footer />
+    </header>
+  );
 }
 
 export default Header
